86aa0cc20c0660b92f8cf2746b8a91d2df3c3523
Lucas Seeger <seegerlucas295@gmail.com>
2020-04-30 15:12:16 -0600

    experimenting with Bootstrap

--- New App/dnd-jrpg/src/App.js
+++ New App/dnd-jrpg/src/App.js
@@ -1,25 +1,42 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-function App() {
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Card from 'react-bootstrap/Card';
+
+
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Col lg={12} style={{ height: "10vh", border: "solid 1px black" }}>Test</Col>
+
+    // <div className="container-fluid h-100">
+    //   <div className="container-fluid col-xs-12 text-center border border-primary h-25">
+    //     <div className="row text-center">
+    //       Monster Dungeon
+    //     </div>
+    //   </div>
+
+    //   <div className="battle-field">
+    //     <div>left</div>
+    //     <div>right</div>
+    //   </div>
+
+
+    //   <div className="row">
+    //     <div className="col-md-6 col-xs-12 border border-primary text-left h-50">
+    //       <div className="container col-md-6 border border-primary" >
+    //       </div>
+    //     </div>
+    //     <div className="col-md-6 col-sm-12 border border-primary text-left">Test!</div>
+    //   </div>
+    // </div>
+
+
   );
 }
 
