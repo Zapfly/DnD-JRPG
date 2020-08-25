b0beba288e9b319250d9b2a3e1db217e3bcaa7dd
nhopfe <55861853+nhopfe@users.noreply.github.com>
2020-08-24 19:23:55 -0600

    cleaned up comments, changed 400 errors to 404s

--- New App/dnd-jrpg/src/server/resources/heroes.py
+++ New App/dnd-jrpg/src/server/resources/heroes.py
@@ -7,33 +7,23 @@ from models.users import UserModel
 
 class Hero(Resource):
     parser = reqparse.RequestParser()
-    # parser.add_argument('username',
-    #     type=str,
-    #     # required=True,
-    #     help="This field cannot be left blank!"
-    # )
     parser.add_argument('heroname',
         type=str,
-        # required=True,
         help="This field cannot be left blank!"
     )
     parser.add_argument('atk',
         type=float,
-        # required=True,
         help="This field cannot be left blank!"
     )
     parser.add_argument('hp',
-        type=float,
-        # required=True,
+        type=float,    
         help="This field cannot be left blank!"
     )
     parser.add_argument('max_hp',
         type=float,
-        # required=True,
         help="This field cannot be left blank!"
     )
     parser.add_argument('sprite',
-        # required=True,
         help="This field cannot be left blank!"
     )
     parser.add_argument('user_id',
@@ -51,18 +41,15 @@ class Hero(Resource):
        
         if hero is None:
             if HeroModel.find_by_user_id_and_heroname(data['user_id'], data['heroname']):
-                return {'message': "A hero with name '{}' already exists.".format(data['heroname'])}, 400
+                return {'message': "A hero with name '{}' already exists.".format(data['heroname'])}, 404
             else:
                 hero = HeroModel(**data)
                 hero.save_to_db()
                 return {'message': "Hero created successfully"}, 200
         else:
             if HeroModel.find_by_user_id_and_heroname(data['user_id'], data['heroname']):
-                return {'message': "A hero with name '{}' already exists.".format(data['heroname'])}, 400
+                return {'message': "A hero with name '{}' already exists.".format(data['heroname'])}, 404
             else:
-                # for item in data:
-                #     print(f'{item} = {hero.json()[item]}')
-                # hero[f'{item}'] = data[f'{item}']
                 hero.heroname = data['heroname']
                 hero.atk = data['atk']
                 hero.hp = data['hp']
@@ -72,10 +59,6 @@ class Hero(Resource):
                 
                 hero.save_to_db()
 
-                # hero_update = HeroModel.find_all_by_user_id_and_heroname(data['user_id'], data['heroname'])
-                # old_hero = HeroModel.find_all_by_user_id_and_heroname(data['user_id'], heroname)
-                # print(hero_update)
-                # print(old_hero)
                 return {'message': "Hero updated"}, 200
 
 
@@ -83,7 +66,7 @@ class Hero(Resource):
     def post(self, heroname):
         data = Hero.parser.parse_args()
         if HeroModel.find_by_user_id_and_heroname(data['user_id'], heroname):
-            return {'message': "A hero with name '{}' already exists.".format(heroname)}, 400
+            return {'message': "A hero with name '{}' already exists.".format(heroname)}, 404
 
 
         hero = HeroModel(**data)
@@ -95,7 +78,7 @@ class Hero(Resource):
         data = Hero.parser.parse_args()
         hero = HeroModel.find_by_user_id_and_heroname(data['user_id'], heroname)
         if hero:
-            return {'message': f'Hero {heroname} found', 'hero': hero.json()}, 200
+            return hero.json(), 200
         return {'message': 'Hero not found'}, 404
 
 
@@ -109,7 +92,7 @@ class Hero(Resource):
             hero.delete_from_db()
             return{"message": "Hero deleted"}, 200
         else:
-            return{"message": "That hero does not exist"}, 400
+            return{"message": "That hero does not exist"}, 404
 
 class HeroList(Resource):
     parser = reqparse.RequestParser()
