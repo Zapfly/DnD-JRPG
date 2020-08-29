449008db9fe37dc1e20c4035a4cb315e1a80f056
nhopfe <55861853+nhopfe@users.noreply.github.com>
2020-08-29 12:03:14 -0600

    refactored punctuation, changed help message, added old_monstername argument

--- New App/dnd-jrpg/src/server/resources/monsters.py
+++ New App/dnd-jrpg/src/server/resources/monsters.py
@@ -26,7 +26,10 @@ class Monster(Resource):
     parser.add_argument('levelname',
         type=str,
         required=True,
-        help="This field cannot be left blank!"
+        help="Every monster must be assigned to a level!"
+    )
+    parser.add_argument('old_monstername',
+        type=str
     )
 
     @jwt_required()
@@ -36,7 +39,7 @@ class Monster(Resource):
         if MonsterModel.find_by_levelname_and_monstername(data['levelname'], data['monstername']):
             return {'message': "A monster with name '{}' already exists in level '{}'.".format(data['monstername'], data['levelname'])}, 404
         
-        monster = MonsterModel(**data)
+        monster = MonsterModel(data['monstername'], data['atk'], data['hp'], data['max_hp'], data['sprite'], data['levelname'])
         try:
             monster.save_to_db()
         except:
@@ -52,19 +55,19 @@ class Monster(Resource):
 
         if monster:
             return monster.json(), 200
-        return {'message': 'Monster not found'}, 404
+        return {'message': 'Monster not found.'}, 404
 
     @jwt_required()
     def put(self):
         data = Monster.parser.parse_args()
-        monster = MonsterModel.find_by_levelname_and_monstername(data['levelname'], data['monstername'])      
+        monster = MonsterModel.find_by_levelname_and_monstername(data['levelname'], data['old_monstername'])      
         if monster is None:
             if MonsterModel.find_by_levelname_and_monstername(data['levelname'], data['monstername']):
                 return {'message': "A monster with name '{}' already exists in level '{}'.".format(data['monstername'], data['levelname'])}, 404
             else:
-                monster = MonsterModel(**data)
+                monster = MonsterModel(data['monstername'], data['atk'], data['hp'], data['max_hp'], data['sprite'], data['levelname'])
                 monster.save_to_db()
-                return {'message': "Monster created successfully"}, 200
+                return {'message': "Monster '{}' created successfully.".format(data['monstername'])}, 201
         else:
             if MonsterModel.find_by_levelname_and_monstername(data['levelname'], data['monstername']):
                 return {'message': "A monster with name '{}' already exists in level '{}'.".format(data['monstername'], data['levelname'])}, 404
@@ -76,7 +79,7 @@ class Monster(Resource):
                 monster.sprite = data['sprite']
                 monster.levelname = data['levelname']              
                 monster.save_to_db()
-                return {'message': "Monster updated"}, 200
+                return {'message': "Monster updated."}, 200
 
     @jwt_required()
     def delete(self):
@@ -86,6 +89,6 @@ class Monster(Resource):
 
         if monster:
             monster.delete_from_db()
-            return{"message": "Monster deleted"}, 200
+            return{"message": "Monster deleted."}, 200
         else:
-            return{"message": "That monster does not exist"}, 404
+            return{"message": "That monster does not exist."}, 404
