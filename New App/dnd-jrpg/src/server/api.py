fb97b20888b59f585e4b8453737acdac4301c887
nhopfe <55861853+nhopfe@users.noreply.github.com>
2020-09-21 17:18:10 -0600

    changed flask jwt to flask jwt extended, added resources

--- New App/dnd-jrpg/src/server/api.py
+++ New App/dnd-jrpg/src/server/api.py
@@ -1,20 +1,19 @@
 from flask import Flask
 from flask_restful import Api
-from flask_jwt import JWT
+from flask_jwt_extended import JWTManager
 from flask_cors import CORS
 import datetime
 from db import db
 
-from security import authenticate, identity
-from resources.users import UserRegister
+from resources.users import UserRegister, User, UserLogin, TokenRefresh
 from resources.heroes import Hero, HeroList
 from resources.levels import Level, LevelList
 from resources.monsters import Monster
 
 app = Flask(__name__)
-app.config['JWT_EXPIRATION_DELTA'] = datetime.timedelta(days=1)
+app.config['JWT_ACCESS_TOKEN_EXPIRES'] = datetime.timedelta(minutes=30)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
-app.config['SQALCHEMY_TRACK_MODIFICATIONS'] = False 
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False 
 app.config['PROPAGATE_EXCEPTIONS'] = True
 
 app.secret_key = 'jose'
@@ -26,14 +25,17 @@ CORS(app)
 def create_tables():
     db.create_all()
 
-jwt = JWT(app, authenticate, identity) #/auth
+jwt = JWTManager(app)
 
 api.add_resource(Level, '/level')
 api.add_resource(LevelList, '/levels')
 api.add_resource(Hero, '/hero/<string:heroname>')
 api.add_resource(HeroList, '/heroes')
 api.add_resource(Monster, '/monster')
-api.add_resource(UserRegister, '/user') #change to <integer:id>
+api.add_resource(UserRegister, '/register')
+api.add_resource(User, '/user')
+api.add_resource(UserLogin, '/login')
+api.add_resource(TokenRefresh, '/refresh')
 
 #api.add_resource(Student, '/student/<string:name>')
  
