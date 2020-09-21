7193ff9471108d8785c00adea7972f5a425602b5
nhopfe <55861853+nhopfe@users.noreply.github.com>
2020-09-21 17:20:26 -0600

    changed flask jwt to flask jwt extended, removed user_id from arguments

--- New App/dnd-jrpg/src/server/resources/heroes.py
+++ New App/dnd-jrpg/src/server/resources/heroes.py
@@ -1,6 +1,6 @@
 import sqlite3
 from flask_restful import Resource, reqparse
-from flask_jwt import JWT, jwt_required
+from flask_jwt_extended import jwt_required, fresh_jwt_required, get_jwt_identity
 
 from models.heroes import HeroModel
 
@@ -22,42 +22,44 @@ class Hero(Resource):
     parser.add_argument('sprite',
         type=str
     )
-    parser.add_argument('user_id',
-        type=int,
-        required=True,
-        help="A user id is required"
-    )
 
-    @jwt_required()
+    @jwt_required
     def post(self, heroname):
         data = Hero.parser.parse_args()
-        if HeroModel.find_by_user_id_and_heroname(data['user_id'], heroname):
+        user_id = get_jwt_identity()
+
+        if HeroModel.find_by_user_id_and_heroname(user_id, heroname):
             return {'message': "A hero with name '{}' already exists.".format(heroname)}, 404
-        hero = HeroModel(**data)
+
+        hero = HeroModel(data['heroname'], data['atk'], data['hp'], data['max_hp'], data['sprite'], user_id)
         hero.save_to_db()
+
         return {'message': 'Hero created successfully', 'hero': hero.json()}
 
-    @jwt_required()
+    @jwt_required
     def get(self, heroname):
-        data = Hero.parser.parse_args()
-        hero = HeroModel.find_by_user_id_and_heroname(data['user_id'], heroname)
+        user_id = get_jwt_identity()
+        hero = HeroModel.find_by_user_id_and_heroname(user_id, heroname)
+
         if hero:
             return hero.json(), 200
         return {'message': 'Hero not found'}, 404
 
-    @jwt_required()
+    @jwt_required
     def put(self, heroname):
         data = Hero.parser.parse_args()
-        hero = HeroModel.find_by_user_id_and_heroname(data['user_id'], heroname)      
+        user_id = get_jwt_identity()
+        hero = HeroModel.find_by_user_id_and_heroname(user_id, heroname) 
+
         if hero is None:
-            if HeroModel.find_by_user_id_and_heroname(data['user_id'], data['heroname']):
+            if HeroModel.find_by_user_id_and_heroname(user_id, data['heroname']):
                 return {'message': "A hero with name '{}' already exists.".format(data['heroname'])}, 404
             else:
-                hero = HeroModel(**data)
+                hero = HeroModel(data['heroname'], data['atk'], data['hp'], data['max_hp'], data['sprite'], user_id)
                 hero.save_to_db()
                 return {'message': "Hero created successfully"}, 201
         else:
-            if HeroModel.find_by_user_id_and_heroname(data['user_id'], data['heroname']):
+            if HeroModel.find_by_user_id_and_heroname(user_id, data['heroname']):
                 return {'message': "A hero with name '{}' already exists.".format(data['heroname'])}, 404
             else:
                 hero.heroname = data['heroname']
@@ -65,15 +67,14 @@ class Hero(Resource):
                 hero.hp = data['hp']
                 hero.max_hp = data['max_hp']
                 hero.sprite = data['sprite']
-                hero.user_id = data['user_id']              
+                hero.user_id = user_id              
                 hero.save_to_db()
                 return {'message': "Hero updated"}, 200
 
-    @jwt_required()
-    def delete(self, heroname):
-        data = Hero.parser.parse_args()
-
-        hero = HeroModel.find_by_user_id_and_heroname(data['user_id'], heroname)
+    @fresh_jwt_required
+    def delete(self, heroname):     
+        user_id = get_jwt_identity()
+        hero = HeroModel.find_by_user_id_and_heroname(user_id, heroname)
 
         if hero:
             hero.delete_from_db()
@@ -82,17 +83,10 @@ class Hero(Resource):
             return{"message": "That hero does not exist"}, 404
 
 class HeroList(Resource):
-    parser = reqparse.RequestParser()
-    parser.add_argument('user_id',
-        type=int,
-        required=True,
-        help="A user id is required"
-    )
-
+    @jwt_required
     def get(self):
-        data = Hero.parser.parse_args()
-
-        hero_list = HeroModel.find_all_by_user_id(data['user_id'])
+        user_id = get_jwt_identity()
+        hero_list = HeroModel.find_all_by_user_id(user_id)
         hero_list_json = {'heroes': [hero.json() for hero in hero_list]}
 
         if len(hero_list_json['heroes']) > 0:
