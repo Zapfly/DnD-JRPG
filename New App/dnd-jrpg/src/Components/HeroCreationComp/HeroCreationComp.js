c3f928bef38ce14e5853985fff036fc9262bbd08
nhopfe <55861853+nhopfe@users.noreply.github.com>
2020-12-14 13:53:03 -0700

    added hero creation functionality

--- New App/dnd-jrpg/src/Components/HeroCreationComp/HeroCreationComp.js
+++ New App/dnd-jrpg/src/Components/HeroCreationComp/HeroCreationComp.js
@@ -1,10 +1,30 @@
 import React, { useState } from 'react';
+import { fetchPostWithAuth } from '../FetchComp/FetchComp.js';
+import { AppContext } from '../../AppContext.js';
 
-const HeroCreationComp = () => {
+const HeroCreationComp = (props) => {
+    const[name, setName] = useState(""); 
+    
+    const appContext = React.useContext(AppContext);
+
+    const createHero = async() => {
+        const res = await fetchPostWithAuth(
+            `${appContext.url}/hero/${name}`, 
+            'POST', 
+            appContext.token, 
+            {"atk": 30, "hp": 200, "max_hp": 200, "sprite": "string"}
+        )
+        props.setCreator(null)
+        console.log(res['message'])
+        return res['message']
+    }
+
     return (
         <div className="hero-creation-container">
             <div>Hero Creation Area</div>
-            <button>Create Hero</button>
+                <label>Name Your Hero:</label>
+                <input className="name-input" value={name} onChange={e => setName(e.target.value)}></input>
+            <button onClick={() => createHero()}>Create Hero</button>
         </div>
     )
 }
