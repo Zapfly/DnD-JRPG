b25a9088c293f6604008bf8e9a4c93b2be50c505
nhopfe <55861853+nhopfe@users.noreply.github.com>
2020-12-14 13:27:28 -0700

    added functions and states for handling api requests and responses

--- New App/dnd-jrpg/src/AppContext.js
+++ New App/dnd-jrpg/src/AppContext.js
@@ -1,27 +1,29 @@
-import React, { useState, useEffect } from 'react';
-import App from './App.js';
+import React, { useState} from 'react';
+// import App from './App.js';
 
 import { Arena } from './scripts/combat.js';
-import { useFetch, useFetchWithAuth } from './Components/FetchComp/FetchComp.js';
+import { fetchFuncAuth, fetchPostWithAuth } from './Components/FetchComp/FetchComp.js';
 
 import Herc from './images/Herccolored.png';
 import Gobo from './images/250px-Chuffy_Lickwound.jpg';
-import CombatComp from './Components/CombatComp/CombatComp.js';
+// import CombatComp from './Components/CombatComp/CombatComp.js';
 
 export const AppContext = React.createContext();
 
 const arena = new Arena();
 
-
 export const ContextProvider = (props) => {
     const [selectedMonster, setSelectedMonster] = useState(null);
     const [attackMessage, setAttackMessage] = useState("");
 
-    const [initFetch, setInitFetch] = useState(false);
-    const [levelList, setLevelList] = useState(null);
-    const [currentLevel, setCurrentLevel] = useState(null);
+    // const [initFetch, setInitFetch] = useState(false);
+    const [currentLevelName, setCurrentLevelName] = useState("");
+    const [currentLevelMonsters, setCurrentLevelMonsters] = useState(null);
+    const [currentHeroData, setCurrentHeroData] = useState(null);
     const [combatLog, setCombatLog] = useState([]);
     const [url, setUrl] = useState("http://localhost:5000");
+    const [token, setToken] = useState(null);
+    const [refreshToken, setRefreshToken] = useState(null);
 
     // useFetchWithAuth()
 
@@ -47,6 +49,43 @@ export const ContextProvider = (props) => {
     //     })
     //     setInitFetch(true);       
     // }
+    const arenaSetupFunc = () => {
+        arena.monsters = [];
+        arena.heroes = [];
+        arena.createHero(currentHeroData.heroname, currentHeroData.atk, currentHeroData.hp, Herc)
+        currentLevelMonsters.forEach(i => {
+            arena.createMonster(i.monstername, i.atk, i.hp, Gobo)
+        })
+        return "Setup Complete";
+    }
+
+    const setupFunc = async () => {
+        await fetchPostWithAuth(`${url}/level`, 'POST', token, {"levelname": "First Level"})
+        await fetchPostWithAuth(`${url}/monster`, 'POST', token, {"monstername": "Boblin1", "atk": 50, "hp": 60, "max_hp": 60, "sprite": "string", "levelname": "First Level"})
+        await fetchPostWithAuth(`${url}/monster`, 'POST', token, {"monstername": "Boblin2", "atk": 50, "hp": 60, "max_hp": 60, "sprite": "string", "levelname": "First Level"})
+    }
+
+    const getCurrentLevel = async (tok) => {
+        const response = await fetchFuncAuth(`${url}/level`, 'GET', tok, {"levelname": "First Level"})
+        setCurrentLevelName(response.levelname)
+        setCurrentLevelMonsters(response.monsters)
+    }
+
+    const getHeroes = async (tok) => {
+        const response = await fetch(`${url}/heroes`, {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${tok}`
+            },
+        });
+        let heroData = response.json()
+        heroData.then((result) => {
+            if (result.heroes[0]) {
+                setCurrentHeroData(result.heroes[0])
+            }
+        })
+    }
 
     const addToLog = (msg) => {
         setCombatLog([...combatLog, ...msg])
@@ -71,12 +110,22 @@ export const ContextProvider = (props) => {
                 setSelectedMonster,
                 attackMessage,
                 setAttackMessage,
-                levelList,
-                setLevelList,
-                currentLevel,
-                setCurrentLevel,
+                currentLevelName,
+                setCurrentLevelName,
+                currentLevelMonsters,
+                setCurrentLevelMonsters,
+                currentHeroData,
+                setCurrentHeroData,
                 url,
                 setUrl,
+                arenaSetupFunc,
+                setupFunc,
+                token,
+                setToken,
+                refreshToken,
+                setRefreshToken,
+                getCurrentLevel,
+                getHeroes,
                 addToLog,
                 deleteCombatants,
             }}>
