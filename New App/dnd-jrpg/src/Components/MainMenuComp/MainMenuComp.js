63e10266c3bc818498243d90e9094857b07cbd0d
nhopfe <55861853+nhopfe@users.noreply.github.com>
2020-12-14 13:57:59 -0700

    added heroCreator, currentHero, and continueHandler functions and UI

--- New App/dnd-jrpg/src/Components/MainMenuComp/MainMenuComp.js
+++ New App/dnd-jrpg/src/Components/MainMenuComp/MainMenuComp.js
@@ -1,10 +1,42 @@
 import React, { useState } from 'react';
+import { AppContext } from '../../AppContext';
 
-const MainMenuComp = () => {
+import HeroCreationComp from '../HeroCreationComp/HeroCreationComp.js';
+
+const MainMenuComp = (props) => {
+    const [creator, setCreator] = useState(null);
+
+    const appContext = React.useContext(AppContext);
+    const heroCreator = () => {
+        if (creator != null) {
+            return < HeroCreationComp
+            setCreator={setCreator}/>
+        } return null;
+    }
+    const currentHero = () => {
+        if (appContext.currentHeroData) {
+            return appContext.currentHeroData.heroname
+        } else {
+            return "This user has no saved heroes"
+        }
+    }
+    const continueHandler = async() => {
+        let promise = new Promise((resolve, reject) => {
+            resolve(appContext.arenaSetupFunc())
+        });
+        await promise;
+        props.setCurrentPage("combat")
+    }
+
     return (
         <div className="main-menu-container">
-            <div>list of existing heroes</div>
-            <button>New Hero</button>
+            <div>Current Hero:</div>
+            <div>{currentHero()}</div>
+            <button onClick={() => continueHandler()}>Continue</button>
+            <button onClick={() => setCreator("newHero")}>New Hero</button>
+            {heroCreator()}
+            {/*remove the following button for production - dev use only */}
+            <button onClick={() => appContext.setupFunc()}>Create Levels in Database</button>
         </div>
     )
 }
